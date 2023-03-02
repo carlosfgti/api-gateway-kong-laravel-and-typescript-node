417742d95db6944ea5b1dcf46595677c1ccec103
Carlos Ferreira <carlos@especializati.com.br>
2023-03-02 11:03:06 -0300

    fix: change connection host, using container name

--- api-ts-node/src/database/connection.ts
+++ api-ts-node/src/database/connection.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm";
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: "postgres",
   port: 5432,
   username: "admin",
   password: "admin",
